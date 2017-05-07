f59acc57b3a88ac0d67dd79fdf0b3815ce7892a5
weicfd <tangmh1996@gmail.com>
2017-05-07 15:23:58 +0800

    First Commit

---
+++ LexicalAnalysis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LexicalAnalysis
+//
+//  Created by Daisy Tang on 17/5/7.
+//  Copyright © 2017年 Daisy Tang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
